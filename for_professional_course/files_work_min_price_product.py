3533662363a820d6cc65111d8c02684126b3d2ba
KarlosGsalva <113985245+KarlosGsalva@users.noreply.github.com>
2023-02-12 16:34:24 +0300

    Find min price of product and shop

---
+++ for_professional_course/files_work_min_price_product.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('prices.csv', encoding='UTF-8') as f:
+    h, *rows = csv.reader(f, delimiter=';')
+
+goods = [(r[0], h[x], r[x]) for r in rows for x in range(1, len(h))]
+cheapest = min(goods, key=lambda x: (int(x[2]), x[1], x[0]))
+
+print(f'{cheapest[1]}: {cheapest[0]}')
