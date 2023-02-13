5df493be733f2c08473e384f06d1b991b2603154
KarlosGsalva <113985245+KarlosGsalva@users.noreply.github.com>
2023-02-14 00:31:33 +0300

    Find min price of product and shop

--- Eduson_git_test.py
+++ Eduson_git_test.py
@@ -1,4 +1,8 @@
-print('First git commit from pycharm')
+import json
 
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
 
-print('feature-1')
+print(json.dumps(countries, indent='   ', separators=(',', ' - '), sort_keys=True))
